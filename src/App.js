fcead4c9a04b6e3695741d066e2d1fb562a40df1
GWeightman <johndoe@example.com>
2021-12-07 23:20:51 +0000

    Heading Relocated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Heading from './Components/heading';
 import Input from './Components/input';
 
 function App() {
     const [artist, setArtist] = useState("")
     const [song, setSong] = useState("")
     const [lyrics, setLyrics] = useState("Lyrics to be found")
-    const [advice, setAdvice] = useState("")
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState({
       error: false,
@@ -20,21 +18,6 @@ function App() {
 
     const changeHandler2 = (event) => {
       setSong(event.target.value)
-    }
-
-    const Adviser = async () => {
-      try {
-        setLoading(true)
-        const response = await fetch("https://api.adviceslip.com/advice")
-        if (response.status !== 200){
-          throw new Error("Not Working")
-        }
-        const data = await response.json()
-        setAdvice(data.slip)
-        setLoading(false)
-      } catch (error){
-        setError({error: true, message: error.message})
-      }
     }
 
     const getter = async () => {
@@ -52,17 +35,19 @@ function App() {
       }
     } 
 
-    useEffect(()=>{
-      Adviser()
-    }, [])
-  
+    const enter = (event) =>{
+      if (event.keyCode == 13){
+        getter()
+      }
+    }
+
     if (error.error){
       return <h1>{error.message}</h1>
     }
     return (
       <div>
-        <Heading advice={advice.advice} loading={loading}/>
-        <Input artist={artist} song={song} changeHandler1={changeHandler1} changeHandler2={changeHandler2} getter={getter} loading={loading} lyrics={lyrics}/> 
+        <h1>Lyric Finder</h1>
+        <Input artist={artist} song={song} changeHandler1={changeHandler1} changeHandler2={changeHandler2} getter={getter} loading={loading} lyrics={lyrics} enter={enter}/> 
       </div>
     )
   }
--- src/Components/input.js
+++ src/Components/input.js
@@ -1,8 +1,8 @@
-const Input = ({artist, song, changeHandler1, changeHandler2, getter, loading, lyrics}) => {
+const Input = ({artist, song, changeHandler1, changeHandler2, getter, loading, lyrics, enter}) => {
     return(
         <div>
             <input type="text" className="UserReq" placeholder="artist" value={artist} onChange={changeHandler1}/>
-            <input type="text" className="UserReq" placeholder="song" value={song} onChange={changeHandler2}/>
+            <input type="text" className="UserReq" placeholder="song" value={song} onChange={changeHandler2} onKeyDown={enter}/>
             <button onClick={getter}>get lyrics</button>
             <br/>
             {loading ? <p>loading...</p>
